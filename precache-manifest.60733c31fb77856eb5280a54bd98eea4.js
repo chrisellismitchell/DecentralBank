b4356f111d83e6dc1cb838e0decd5309c14b9978
chrisellismitchell <sydarious@gmail.com>
2021-09-27 09:48:23 -0400

    Updates

---
+++ precache-manifest.60733c31fb77856eb5280a54bd98eea4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6fae787c8549db113bd",
+    "url": "/DecentralBank/static/css/main.bd3fdae5.chunk.css"
+  },
+  {
+    "revision": "e6fae787c8549db113bd",
+    "url": "/DecentralBank/static/js/main.e6fae787.chunk.js"
+  },
+  {
+    "revision": "afa54904a9c9e3d9343d",
+    "url": "/DecentralBank/static/css/1.60b8a8be.chunk.css"
+  },
+  {
+    "revision": "afa54904a9c9e3d9343d",
+    "url": "/DecentralBank/static/js/1.afa54904.chunk.js"
+  },
+  {
+    "revision": "3964b3b1211337dd06a3",
+    "url": "/DecentralBank/static/js/runtime~main.3964b3b1.js"
+  },
+  {
+    "revision": "da70255120845f3066c5bdcc7367f259",
+    "url": "/DecentralBank/static/media/tether.da702551.png"
+  },
+  {
+    "revision": "f9ed15ce64b1bd3cc8d887b07a0315e5",
+    "url": "/DecentralBank/index.html"
+  }
+];
